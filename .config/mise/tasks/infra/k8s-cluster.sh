67b7e53141499938b0ea7c49924ab286b90a6b50
Brandt Kinzle <114622981+bkinzle@users.noreply.github.com>
2025-11-10 09:51:54 -0600

    feat: add local registry and k8s-cluster set up

---
+++ .config/mise/tasks/infra/k8s-cluster.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#MISE description="Sets up a local kind kubernetes cluster"
+
+mkdir -p "k8s/.local-registry/localhost:${LOCAL_REGISTRY_PORT}" "k8s/.local-registry/${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"
+
+cat <<EOF > "k8s/.local-registry/localhost:${LOCAL_REGISTRY_PORT}/hosts.toml"
+  [host."http://${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"]
+    skip_verify = true
+EOF
+
+cat <<EOF > "k8s/.local-registry/${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}/hosts.toml"
+  server = "http://${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"
+  
+  [host."http://${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"]
+    capabilities = ["pull", "resolve"]
+    skip_verify = true
+EOF
+
+mkdir -p k8s/.cluster-config
+envsubst < ./k8s/kind-config.yaml > k8s/.cluster-config/kind-config.yaml
+kind create cluster --name ${STACK_NAME} --config k8s/.cluster-config/kind-config.yaml
---
+++ .config/mise/tasks/infra/local-registry.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#MISE description="Set up a local oci artifact registry"
+#MISE depends=["check-kubecontext"]
+
+docker run \
+  -d --restart=always -p "127.0.0.1:${LOCAL_REGISTRY_PORT}:${INCLUSTER_REGISTRY_PORT}" --name "${LOCAL_REGISTRY_NAME}" \
+  -e REGISTRY_HTTP_ADDR=:${INCLUSTER_REGISTRY_PORT} \
+  registry:3
+
+# connect the registry to the cluster network
+docker network connect "kind" "${LOCAL_REGISTRY_NAME}"
+
+# document the local registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${LOCAL_REGISTRY_PORT}"
+    hostFromContainerRuntime: "${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"
+    hostFromClusterNetwork: "${LOCAL_REGISTRY_NAME}:${INCLUSTER_REGISTRY_PORT}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
