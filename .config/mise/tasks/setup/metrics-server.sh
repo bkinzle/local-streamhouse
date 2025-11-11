83441c576e16ed49f1d1eef1a56a56d9b805d525
Brandt Kinzle <114622981+bkinzle@users.noreply.github.com>
2025-11-10 21:43:40 -0600

    feat: add metrics-server and minio-operator

---
+++ .config/mise/tasks/setup/metrics-server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#MISE description="Sets up a Kubernetes Metrics Server"
+#MISE depends=["check:kubecontext"]
+
+helm upgrade --repo https://kubernetes-sigs.github.io/metrics-server/ metrics-server metrics-server \
+  --version 3.13.0 \
+  --namespace kube-system \
+  --install \
+  --atomic \
+  --values - <<EOF
+metrics:
+  enabled: true
+apiService:
+  caBundle: 
+args:
+- --kubelet-insecure-tls
+EOF
---
+++ .config/mise/tasks/setup/minio-operator.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#MISE description="Sets up a MinIO Operator"
+#MISE depends=["check:kubecontext"]
+
+helm upgrade --repo https://operator.min.io operator operator \
+  --version ${MINIO_HELM_VERSION} \
+  --namespace minio-operator \
+  --create-namespace \
+  --install \
+  --atomic \
+  --values - <<EOF
+operator:
+  env:
+    - name: OPERATOR_STS_AUTO_TLS_ENABLED
+      value: "off"
+    - name: OPERATOR_STS_ENABLED
+      value: "on"
+    - name: WATCHED_NAMESPACE
+      value: "datalake"
+EOF
