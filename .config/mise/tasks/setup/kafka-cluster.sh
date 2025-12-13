ec9808915f619b152f2fd806a96625660d66afee
Brandt Kinzle <114622981+bkinzle@users.noreply.github.com>
2025-12-13 15:10:07 -0700

    feat: added kafka broker rack awareness

--- .config/mise/tasks/setup/kafka-cluster.sh
+++ .config/mise/tasks/setup/kafka-cluster.sh
@@ -8,7 +8,7 @@ kubectl apply -n streamhouse -f - <<EOF
 apiVersion: kafka.strimzi.io/v1
 kind: KafkaNodePool
 metadata:
-  name: kraft-controller
+  name: kraft-controller-pool-a
   labels:
     strimzi.io/cluster: kafka
 spec:
@@ -23,17 +23,27 @@ spec:
         size: 250Mi
         kraftMetadata: shared
         deleteClaim: false
+  template:
+    pod:
+      topologySpreadConstraints:
+      - maxSkew: 1
+        topologyKey: topology.kubernetes.io/zone
+        whenUnsatisfiable: ScheduleAnyway
+        labelSelector:
+          matchLabels:
+            strimzi.io/cluster: kafka
+            strimzi.io/controller-role: "true"
 EOF
 
 kubectl apply -n streamhouse -f - <<EOF
 apiVersion: kafka.strimzi.io/v1
 kind: KafkaNodePool
 metadata:
-  name: broker
+  name: broker-pool-a
   labels:
     strimzi.io/cluster: kafka
 spec:
-  replicas: 4
+  replicas: 3
   roles:
     - broker
   storage:
@@ -49,6 +59,50 @@ spec:
         type: persistent-claim
         size: 3Gi
         deleteClaim: false
+  template:
+    pod:
+      topologySpreadConstraints:
+      - maxSkew: 1
+        topologyKey: topology.kubernetes.io/zone
+        whenUnsatisfiable: ScheduleAnyway
+        labelSelector:
+          matchLabels:
+            strimzi.io/cluster: kafka
+            strimzi.io/broker-role: "true"
+---
+apiVersion: kafka.strimzi.io/v1
+kind: KafkaNodePool
+metadata:
+  name: broker-pool-b
+  labels:
+    strimzi.io/cluster: kafka
+spec:
+  replicas: 2
+  roles:
+    - broker
+  storage:
+    type: jbod
+    volumes:
+      - id: 0
+        type: persistent-claim
+        size: 250Mi
+        # Indicates that this directory will be used to store Kraft metadata log
+        kraftMetadata: shared
+        deleteClaim: false
+      - id: 1
+        type: persistent-claim
+        size: 3Gi
+        deleteClaim: false
+  template:
+    pod:
+      topologySpreadConstraints:
+      - maxSkew: 1
+        topologyKey: topology.kubernetes.io/zone
+        whenUnsatisfiable: ScheduleAnyway
+        labelSelector:
+          matchLabels:
+            strimzi.io/cluster: kafka
+            strimzi.io/broker-role: "true"
 EOF
 
 kubectl apply -n streamhouse -f - <<EOF
@@ -86,8 +140,19 @@ spec:
       message.max.bytes: 33555555
       # auto.create.topics.enable: false
       # delete.topic.enable: true
+      # This replica selector plugin enables consumers to fetch from closest replica (not just the leader)
+      # In addition to the broker configuration, you also need to specify the client.rack option in your consumers
+      replica.selector.class: org.apache.kafka.common.replica.RackAwareReplicaSelector
     jmxOptions: {}
+    # Broker pods use init containers to collect the topology label from the k8s cluster nodes and assigns a rack ID to each broker
+    # Brokers will spread partition replicas across as many different racks as possible, improving resiliency, availability and reliability
+    rack:
+      topologyKey: topology.kubernetes.io/zone
   entityOperator:
     topicOperator: {}
     userOperator: {}
+  kafkaExporter:
+    topicRegex: ".*"
+    groupRegex: ".*"
+  # cruiseControl:
 EOF
